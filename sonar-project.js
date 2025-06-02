91c458752a4f0269674ad4b38f33d1804564fbf3
MANOHARA AR <84583659+Manohara777@users.noreply.github.com>
2025-06-02 19:36:36 +0530

    Update sonar-project.js
    Update

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.77.152:9000/',
+  serverUrl: 'http://3.110.136.135:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'Dec@2018',
+	    'sonar.password': 'Ammanallur@123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
